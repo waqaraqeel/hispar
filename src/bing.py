438b6ba4510a02a013ff09dbf38c48999821c5d3
Waqar Aqeel <waqeel@cs.duke.edu>
2020-02-15 12:31:56 -0500

    landing page in all cases

--- src/bing.py
+++ src/bing.py
@@ -45,9 +45,11 @@ while printed < target:
         response.raise_for_status()
     except:
         break
+
     search_results = response.json()
     if "webPages" not in search_results:
         break
+
     for rank, ans in enumerate(search_results["webPages"]["value"]):
         url = ans["url"]
         if url not in uniques:
@@ -56,7 +58,7 @@ while printed < target:
                 print(f"{site_rank} 0 {url}")
                 landing_found = True
             elif printed == target - 1 and landing and not landing_found:
-                print(f"{site_rank} 0 {landing}")
+                break
             else:
                 print(f"{site_rank} {offset + rank + 1} {url}")
             printed += 1
@@ -68,3 +70,7 @@ while printed < target:
         break
     offset += len(search_results["webPages"]["value"])
     time.sleep(0.02)
+
+
+if landing and not landing_found:
+    print(f"{site_rank} 0 {landing}")
